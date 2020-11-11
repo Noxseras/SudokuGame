3c863a63b6fceb739e761577725a3d13d3cbf5bc
Noxseras <tzouliano10@gmail.com>
2020-11-11 02:03:59 +0200

    Updated Sudoku game

--- Sudoku_Game.py
+++ Sudoku_Game.py
@@ -1,89 +1,82 @@
-import numpy as np
-
-sd = np.array([[0, 2, 0, 3, 0, 0, 9, 0, 0],
-               [0, 0, 0, 7, 5, 2, 0, 3, 0],
-               [1, 0, 5, 0, 8, 0, 2, 0, 6],
-               [6, 0, 0, 0, 3, 8, 7, 5, 1],
-               [0, 0, 2, 0, 4, 7, 0, 0, 3],
-               [0, 0, 0, 6, 0, 0, 4, 0, 0],
-               [0, 0, 9, 0, 0, 3, 5, 1, 0],
-               [5, 8, 1, 0, 0, 4, 3, 0, 0],
-               [4, 7, 0, 1, 0, 0, 0, 2, 9]])
-
-print(sd)
+from termcolor import colored
 
 
-# Function to check if the user inputs letters instead of integers
-def input_check():
-    global value, line, column
-    while True:
-        try:
-            value = int(input("Enter a value(1-9): "))
-            line = int(input("Enter line(0-8): "))
-            column = int(input("Enter column(0-8): "))
-            if not value_range(value, line, column):
-                continue
-            return value, column, line
-        except ValueError:
-            print("Please enter only numbers, not letters or symbols!")
+class Sudoku:
+    def __init__(self):
+        self.board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
 
+    # Print board and make it look better
+    def display_board(self, bo):
+        for i in range(len(bo)):
+            if i % 3 == 0 and i != 0:
+                print(" - - - - - - - - -")
+            for j in range(len(bo[0])):
+                if j % 3 == 0 and j != 0:
+                    print(" | ", end="")
+                if j == 8:
+                    print(bo[i][j])
+                else:
+                    print(str(bo[i][j]) + " ", end="")
 
-# Check the range of the value
-def value_range(value, line, column):
-    if value < 1 or value > 9:
-        print("Value out of range!")
-        return False
-    elif line < 0 or line > 9:
-        print("Line out of range!")
-        return False
-    elif column < 0 or column > 9:
-        print("Column out of range!")
-        return False
-    else:
+    # Check the range of the value
+    def value_range(self, l, c, v):
+        l = l - 1
+        c = c - 1
+        if l < 0 or c < 0 or l > 8 or c > 8 or v < 1 or v > 9:
+            print("Invalid option, number already exists")
+            return False
+        """ Check areas and sub-areas"""
+        for i in range(9):
+            if self.board[l][i] == v:
+                print("LINE")
+                return False
+            if self.board[i][c] == v:
+                print("COLUMN")
+                return False
+            if not self.subareas(l, c, v):
+                return False
+            else:
+                self.board[l][c] = v
+                return True
+        return True
+
+    # check for sub areas
+    def subareas(self, l, c, v):
+        ia = l // 3  # divide every number of the row with 3, if position [0, 4], its in [0, 1](min and max:[0,2]-[2,2])
+        ja = c // 3
+        for i in range(ia * 3, ia * 3 + 3):
+            for j in range(ja * 3, ja * 3 + 3):
+                if self.board[i][j] == v:
+                    return False
+        return True
+
+    def completed(self):
+        for r in range(0, 9):
+            for c in range(0, 9):
+                if self.board[r][c] == 0:
+                    return False
         return True
 
 
-input_check()
+sudoku = Sudoku()
+while not sudoku.completed():
+    sudoku.display_board(sudoku.board)
+    x = int(input("Row: "))
+    y = int(input("Col: "))
+    val = int(input("Value: "))
+    if not sudoku.value_range(x, y, val):
+        print("Wrong position")
 
-
-# check if value is on line or column
-def check_lines_rows():
-    global line, value, column
-    for i in range(9):
-        if sd[line][i] == value:
-            print(f"Number in row {line} already exists! Try again!")
-            return sd
-        if sd[i][column] == value:
-            print(f"Number in column {column} already exists!")
-            return sd
-
-
-check_lines_rows()
-
-
-# check if the cell is empty or not( can store if and only the cell is 0) We do this, not to overwrite the cell
-def cell_is_empty():
-    global line, value, column
-    cell_boolean = False
-    for i in range(9):
-        if sd[line][i] != 0:
-            print(f"Cell in line {line} not emtpy!")
-            return sd
-        if sd[i][column] != 0:
-            print(f"Cell in column {column} is not empty!")
-            return sd
-        if sd[line][i] == 0 and sd[column][i] == 0:
-            print("Cell is empty!")
-            cell_boolean = True
-            add_to_cell(cell_boolean)
-            return sd, cell_boolean
-
-
-# Store the value the user entered, in the cell
-def add_to_cell(cell_boolean):
-    if cell_boolean:
-        sd[line][column] = value
-        print(sd)
-
-
-cell_is_empty()
+sudoku.display_board(sudoku.board)
+print("Great job!")
+exit()
